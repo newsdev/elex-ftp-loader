746159bdf9fddd8d431323061e1b8233470b9006
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-11-02 14:53:48 -0400

    Fix

--- scripts/prd/_results.sh
+++ scripts/prd/_results.sh
@@ -1,13 +1,13 @@
 function set_live_tables {
-    cat fields/results.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+    cat fields/results.txt | psql -h localhost -U elex -d elex_$RACEDATE
 }
 
 function set_temp_tables {
-    cat fields/results_temp.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+    cat fields/results_temp.txt | psql -h localhost -U elex -d elex_$RACEDATE
 }
 
 function load_national_results {
-    elex-ftp $RACEDATE | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "COPY results_temp FROM stdin DELIMITER ',' CSV HEADER;"
+    elex-ftp $RACEDATE | psql -h localhost -U elex -d elex_$RACEDATE -c "COPY results_temp FROM stdin DELIMITER ',' CSV HEADER;"
 }
 
 function copy_results {
--- scripts/stg/_results.sh
+++ scripts/stg/_results.sh
@@ -1,15 +1,19 @@
-function set_live_tables {
-    cat fields/results.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+function set_live_tables {e
+    echo "Setting live results tables."
+    cat fields/results.txt | psql -h localhost -U elex -d elex_$RACEDATE
 }
 
 function set_temp_tables {
-    cat fields/results_temp.txt | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE
+    echo "Setting temp results tables."
+    cat fields/results_temp.txt | psql -h localhost -U elex -d elex_$RACEDATE
 }
 
 function load_national_results {
-    elex-ftp $RACEDATE | psql -h $ELEX_DB_HOST -U elex -d elex_$RACEDATE -c "COPY results_temp FROM stdin DELIMITER ',' CSV HEADER;"
+    echo "Getting data."
+    elex-ftp $RACEDATE | psql -h localhost -U elex -d elex_$RACEDATE -c "COPY results_temp FROM stdin DELIMITER ',' CSV HEADER;"
 }
 
 function copy_results {
+    echo "Copying results."
     psql elex_$RACEDATE -c "TRUNCATE results CASCADE; INSERT INTO results SELECT * FROM results_temp;"
 }
